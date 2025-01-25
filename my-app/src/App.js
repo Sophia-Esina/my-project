b95c211650df8a43f42d49e3a1514520fcdbf820
Sophia-Esina <esina034@gmail.com>
2025-01-25 18:10:36 +0300

    developing instructions in app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,73 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from 'react';
+import styles from './app.module.css';
+import data from './data.json';
 
-function App() {
-  const currentYear = new Date().getFullYear(); //императивный стиль
+export const App = () => {
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Current Year: {currentYear}</p>
-      </header>
-    </div>
-  ); //декларативный стиль
-}
+	const clickBack = () => {
+		if (activeIndex > 0) {
+			setActiveIndex(activeIndex - 1);
+		}
+	};
 
-export default App;
+	const clickForward = () => {
+		if (activeIndex < steps.length - 1) {
+			setActiveIndex(activeIndex + 1);
+		}
+	};
+
+	const clickStartOver = () => {
+		setActiveIndex(0);
+	};
+
+	const isFirstStep = activeIndex === 0;
+	const isLastStep = activeIndex === steps.length - 1;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+						Контент соответственный шагу. Сейчас активен шаг 3
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((step, index) => (
+							<li
+								key={index}
+								className={`${styles['steps-item']} ${index < activeIndex ? styles.done : ''}
+							${index === activeIndex ? styles.active : ''}`}
+							>
+								<button
+									className={styles['steps-item-button']}
+									onClick={() => setActiveIndex(index)}
+								>
+									{index + 1}
+								</button>
+								{step.title}
+							</li>
+						))}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							className={styles.button}
+							onClick={clickBack}
+							disabled={isFirstStep}
+						>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={isLastStep ? clickStartOver : clickForward}
+						>
+							{isLastStep ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
